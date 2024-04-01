374c67b98826a018c25ba4261f40d7075bdd2b1c
ETwilight <hellobrian18@163.com>
2024-04-01 01:31:00 -0500

    change password, update iframe

--- src/components/SecureView.vue
+++ src/components/SecureView.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!--Link to dist/secure.html (since it is on server no need for dist) via IFrame-->
-        <iframe src="secure.html" width="100%" height="60%"></iframe>
+        <iframe src="secure.html" width="100%" height="100%"></iframe>
     </div>
 </template>
 
