1d9e66ab06494d5ca44e974890da88ba5f47cc40
ETwilight <hellobrian18@163.com>
2024-04-01 01:12:26 -0500

    Change Secure Way, add gitignore to hide secure contents

--- src/components/SecureView.vue
+++ src/components/SecureView.vue
@@ -1,31 +1,12 @@
 <template>
     <div>
-        <input type="password" v-model="password" placeholder="Enter password" />
-        <button @click="checkPassword">Submit</button>
-        <div v-if="showContent">
-            <!-- Your secure content goes here -->
-        </div>
+        <!--Link to dist/secure.html via IFrame-->
+        <iframe src="dist/secure.html" width="100%" height="60%"></iframe>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            password: '',
-            showContent: false
-        };
-    },
-    methods: {
-        checkPassword() {
-            // Replace 'password123' with your desired password
-            if (this.password === 'password123') {
-                this.showContent = true;
-            } else {
-                // Redirect back to App.vue or any other component
-                this.$router.push('/app');
-            }
-        }
-    }
-};
+    export default {
+        name: 'SecureView'
+    };
 </script>
