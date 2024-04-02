e80fca06265dbff42dfe79d251eae0e2798b79c9
ETwilight <hellobrian18@163.com>
2024-04-02 04:37:12 -0500

    major commitment

--- postprocess.py
+++ postprocess.py
@@ -6,10 +6,10 @@ def main():
     with open(secure_html_path, 'r') as file:
         content = file.read()
 
-    content = content.replace("This page is password protected.", "Restrict to URFO Musicians and Staff Members only.")
+    content = content.replace("This page is password protected.", "Restrict to URFO Musicians and Staff Members Only.")
     content = content.replace("Protected Page", "URFO Internal Portal")
-    content = content.replace("background:#000;", "background:#800000;")
-    content = content.replace(".box{max-width:24rem;width:100%;background:var(--gray-800);padding:1rem;border-radius:.125rem;margin:0 auto;height:170px}", ".box{max-width:24rem;width:100%;background:var(--gray-800);padding:1rem;border-radius:.125rem;margin:0 auto;height:200px}")
+    content = content.replace("background:#000;", "background:#7e0000;")
+    content = content.replace(".box{max-width:24rem;width:100%;background:var(--gray-800);padding:1rem;border-radius:.125rem;margin:0 auto;height:170px}", ".box{max-width:24rem;width:100%;background:var(--gray-800);padding:1rem;border-radius:.125rem;margin:0 auto;height:220px}")
 
     with open(secure_html_path, 'w') as file:
         file.write(content)
